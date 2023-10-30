91fba666bb1d78f082a547e30dbd9b83d74e441e
Gideon Edoghotu <edoghotugiddy@gmail.com>
2023-10-30 22:26:06 +0100

    updated

--- Chrome_Extension1/script.js
+++ Chrome_Extension1/script.js
@@ -1,22 +1,41 @@
 const saveBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
-// let msgEl = document.getElementById("message-el")
-// let sumEl = document.getElementById("sum-el")
+let ulEl = document.getElementById("ul-el")
+let container = document.getElementById("container")
+const buyBtn = document.getElementById("buy-btn")
+
 // let cardsEl = document.getElementById("cards-el")
 // let greetingEl = document.getElementById("greeting-el")
 // let playerEl = document.getElementById("player-el")
 
-let myLeads = []
-let message = ""
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 
 saveBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
-    console.log(myLeads);
+    // console.log(myLeads);
 })
 
+function buy(){
+    container.innerHTML += "<p>Thank you<p/>"
+    console.log(9);
+}
+
+
 // for (let i = 0; i < myLeads.length; i++){
-//     console.log(myLeads[i]);
+//     ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
 // }
+
+for (let i = 0; i < myLeads.length; i++){
+    //create element
+    //set text element
+    //append to ul
+    const li = document.createElement("li")
+    li.textContent = myLeads[i]
+    ulEl.append(li)
+}
+
+container.innerHTML = '<button onClick="buy()">Buy</button>'
+
 
 
 // getSubBtn.addEventListener("click", Sub)
